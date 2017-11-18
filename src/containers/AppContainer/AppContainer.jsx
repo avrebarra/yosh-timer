cc4177fb8081a14b9f3a5b936d495384d15a3b3f
Avreghly Barra <avreghly@gmail.com>
2017-11-18 14:10:03 +0700

    fixing grand app structure

--- src/containers/AppContainer/AppContainer.jsx
+++ src/containers/AppContainer/AppContainer.jsx
@@ -2,20 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import App from 'components/App'
 
-class AppContainer extends Component {
-  constructor(props){
-    super(props)
-
-    this.state = {appState : 'input', time : null}
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
-    );
-  }
-}
+const AppContainer = () => (
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+);
 
 export default AppContainer;
